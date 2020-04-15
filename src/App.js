2ef79b6c648a84c99ee82fbfe8cb134fb8f2e2b6
gracechoe <gracechoe925@gmail.com>
2020-04-15 15:16:59 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component, useRef, useState, Suspense } from 'react'
+import Table from './Table'
+import Form from './Form'
+import FileInput from './FileInput'
+import { Canvas, useFrame, useLoader } from 'react-three-fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+
+const Box = props => {
+    // This reference will give us direct access to the mesh
+    const mesh = useRef()
+    
+    // Set up state for the hovered and active state
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    
+    // Rotate mesh every frame, this is outside of React without overhead
+    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+    
+    return (
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? [1, 1, 1] : [1, 1, 1]}
+        onClick={e => setActive(!active)}
+        onPointerOver={e => setHover(true)}
+        onPointerOut={e => setHover(false)}>
+        <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
+        <meshStandardMaterial attach="material" color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+}
+
+const Asset = url => {
+    const obj = useLoader(OBJLoader, url)
+    return <mesh object={obj.scene} dispose={null} />
+}
+
+class App extends Component {
+    state = {
+        characters: [],
+        projectName: "",
+        file: null,
+    }
+    
+    removeCharacter = index => {
+        const { characters } = this.state
+        
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            })
+        })
+    }
+
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character]})
+    }
+    
+    handleFileSubmit = fileState => {
+        this.setState({
+            projectName: fileState.projectName,
+            file: fileState.file,
+        })
+
+    }
+    // handleSubmit = file => {
+    //     this.setState({ uploadFile: file})
+    // }
+    render() {
+        const { characters } = this.state
+
+        return (
+            <div className="container">
+                <h1>Table Example</h1>
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit}/>
+                <br />
+                <h1>AR Smart Worker Web Portal</h1>
+                <FileInput handleSubmit={this.handleFileSubmit}/>
+            </div>
+
+        )
+    }
+}
+
+export default App
---
+++ src/FileInput.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+
+class FileInput extends Component {
+    initialState = {
+        projectName: '',
+        file: '',
+    }
+    state = this.initialState
+
+    // fileInput = React.createRef();
+
+    handleNameChange = event => {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value, 
+        })
+    }
+
+    handleFileChange = event => {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value, 
+        })
+
+        // alert(
+        //     `Selected file - ${this.fileInput.current.files[0].name}`
+        // );
+    }
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
+
+    // handleSubmit(event) {
+    //   event.preventDefault();
+    //   alert(
+    //     `Selected file - ${this.fileInput.current.files[0].name}`
+    //   );
+    // }
+  
+    render() {
+        const { projectName, file } = this.state
+        return (
+            <form>
+                <label htmlFor="projectName">Project Name</label>
+                <input
+                    type="text"
+                    name="projectName"
+                    id="projectName"
+                    value={projectName}
+                    onChange={this.handleNameChange} />
+                <label htmlFor="file">Upload file:</label>
+                <input 
+                    type="file"
+                    name="file" 
+                    id="file" 
+                    value={file}
+                    onChange={this.handleFileChange}
+                    />
+                {/* <input type="file" id="file" ref={this.fileInput} onChange={this.handleChange}/> */}
+                <input type="button" value="Submit" onClick={this.submitForm} />
+            </form>
+      );
+    }
+  }
+
+export default FileInput;
