9092ef2e7def0ad07a63cfcdef1c2e069978e825
gracechoe <gracechoe925@gmail.com>
2020-05-27 14:07:21 -0700

    Fix API call error

--- src/App.js
+++ src/App.js
@@ -44,80 +44,82 @@ const App = () => {
   const bearerKey =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZHdqaWFuZ0B1Y2kuZWR1IiwiaWF0IjoxNTg5MjA5OTA1LCJleHAiOjE1ODkyOTYzMDV9.gNpipNY8vbi5BRa1Y2zy8Np_6QLJSQiHRMK_5lvFYVA";
 
-  const postData = (url = "", data = {}, contentType) => {
-    console.log(data);
-    const formData = new FormData();
-    formData.append("data", data.data);
-    formData.append("name", data.name);
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ", " + pair[1]);
-    }
+  const postModelData = async (url = "", data = {}) => {
     const postHeader = {
       Accept: "application/json",
-      // "Content-Type": contentType,
       Authorization: bearerKey,
     };
-    fetch(url, {
+
+    const modelData = new FormData();
+    modelData.append("data", data.data);
+    modelData.append("name", data.name);
+    const response = await fetch(url, {
       method: "POST",
       headers: postHeader,
-      body: formData,
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        return data;
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
+      body: modelData,
+    });
+
+    return response.json();
   };
 
-  const postModel = () => {
-    console.log("At postModel");
-    var assetID = 0;
+  const postStepsData = async (url = "", data = {}) => {
+    const postHeader = {
+      Accept: "application/json",
+      Authorization: bearerKey,
+      "Content-Type": "application/json",
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: postHeader,
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  };
+
+  const postModel = async () => {
     const body = {
       name: state.projectName,
       data: state.file, // regular File object
     };
-    postData(
+    const assetData = await postModelData(
       "http://70.187.182.170:3000/api/assets/insert",
       body,
       "application/x-www-form-urlencoded"
-    ).then((data) => {
-      console.log(data);
-    });
+    );
+    console.log(assetData);
+
+    return assetData.id;
   };
 
-  const postSteps = (steps) => {
+  const postSteps = async (steps, assetId) => {
     console.log("POSTING STEPS");
     for (var i = 0; i < steps.length; i++) {
       const textData = {
         text: steps[i].text,
-        assetID: 2,
+        assetID: assetId,
         step: i + 1,
         organization: "UCI",
       };
       const partsData = {
         name: steps[i].part,
-        assetID: 2,
+        assetID: assetId,
         step: i + 1,
         organization: "UCI",
       };
       console.log(textData);
       console.log(partsData);
-      postData(
+      await postStepsData(
         "http://70.187.182.170:3000/api/steps/insert",
-        textData,
-        "application/json"
+        textData
       ).then((data) => {
         console.log(data);
       });
       if (steps[i].part !== "None") {
-        postData(
+        await postStepsData(
           "http://70.187.182.170:3000/api/parts/insert",
-          partsData,
-          "application/json"
+          partsData
         ).then((data) => {
           console.log(data);
         });
@@ -125,11 +127,14 @@ const App = () => {
     }
   };
 
-  const handleTutorialSubmit = (steps) => {
-    postModel();
-    // postSteps(steps);
-    // setState(initialState);
-    alert("New tutorial successfully created");
+  const handleTutorialSubmit = async (steps) => {
+    const assetId = await postModel();
+    console.log(assetId);
+    if (assetId) {
+      await postSteps(steps, assetId);
+    }
+    setState(initialState);
+    alert("New tutorial successfully created.");
   };
 
   const portalHeader = " AR Smart Worker Web Portal";
